494efded2b4286c51cf013c7a75fcab1eed37f21
chenxinxu <1031307782@qq.com>
2025-04-10 15:20:18 +0800

    add color for log level prefix

--- include/utils/logger.h
+++ include/utils/logger.h
@@ -9,6 +9,38 @@
 #include <sstream>
 #include <chrono>
 
+enum loglvl
+{
+    FATL = 0,
+    ERRO = 1,
+    WARN = 2,
+    INFO = 3,
+    DBUG = 4,
+    NONE = 5
+};
+
+// 预定义的日志等级颜色
+const std::string lvl_clr_list[] 
+{
+    "\033[31m", // red
+    "\033[33m", // yellow
+    "\033[34m", // blue
+    "\033[32m", // green
+    "\033[35m", // magenta
+    "\033[0m",  // none
+};
+const std::string reset_clr = "\033[0m"; // reset color
+// 预定义的日志等级前缀
+const std::string lvl_pre_list[] 
+{
+    "FATL",
+    "ERRO",
+    "WARN",
+    "INFO",
+    "DBUG",
+    "NONE"
+};
+
 template<typename T>
 void append_to_stream(std::ostringstream& ss, const T& value) {
     ss << value << " ";
@@ -22,6 +54,7 @@ void append_to_stream(std::ostringstream& ss, const T& value, const Args&... arg
 template <typename... Args>
 void log(
     const std::string& file, const int& line, 
+    loglvl lvl,
     const std::string& pre, Args... args
 )
 {
@@ -41,6 +74,7 @@ void log(
 
     std::ostringstream log_msg;
     log_msg << "[" << timestamp << "]"
+            << lvl_clr_list[lvl] << "[" << lvl_pre_list[lvl] << "]" << reset_clr
             << "[" << pre << "]"
             << " ";
     // use recursive template func to expand args
@@ -52,11 +86,11 @@ void log(
     std::cerr << log_msg.str() << std::endl;
 }
 
-#define LOGD(...) log(__FILE__, __LINE__, "    ", __VA_ARGS__)
-#define DBUG(...) log(__FILE__, __LINE__, "DBUG", __VA_ARGS__)
-#define INFO(...) log(__FILE__, __LINE__, "INFO", __VA_ARGS__)
-#define WARN(...) log(__FILE__, __LINE__, "WARN", __VA_ARGS__)
-#define ERRO(...) log(__FILE__, __LINE__, "ERRO", __VA_ARGS__)
-#define FATL(...) log(__FILE__, __LINE__, "FATL", __VA_ARGS__)
+#define FATL(...) log(__FILE__, __LINE__, loglvl::FATL, "", __VA_ARGS__);
+#define ERRO(...) log(__FILE__, __LINE__, loglvl::ERRO, "", __VA_ARGS__);
+#define WARN(...) log(__FILE__, __LINE__, loglvl::WARN, "", __VA_ARGS__);
+#define INFO(...) log(__FILE__, __LINE__, loglvl::INFO, "", __VA_ARGS__);
+#define DBUG(...) log(__FILE__, __LINE__, loglvl::DBUG, "", __VA_ARGS__);
+#define LOGD(...) log(__FILE__, __LINE__, loglvl::NONE, "", __VA_ARGS__);
 
 #endif
